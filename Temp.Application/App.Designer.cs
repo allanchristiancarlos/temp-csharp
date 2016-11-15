6a60c9fd190392d107d40ede109ea4b6dd69b090
Allan Christian Carlos <allan.carlos@nowcom.com>
2016-11-15 12:19:32 +0800

    implementation of search by zip code

---
+++ Temp.Application/App.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Temp.App
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtboxZipCode = new System.Windows.Forms.TextBox();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(97, 110);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(83, 31);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtboxZipCode
+            // 
+            this.txtboxZipCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxZipCode.Location = new System.Drawing.Point(12, 82);
+            this.txtboxZipCode.Name = "txtboxZipCode";
+            this.txtboxZipCode.Size = new System.Drawing.Size(260, 22);
+            this.txtboxZipCode.TabIndex = 1;
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(83, 59);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(112, 20);
+            this.lblEnter.TabIndex = 2;
+            this.lblEnter.Text = "Enter ZipCode";
+            this.lblEnter.Click += new System.EventHandler(this.lblEnterZipCode_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.txtboxZipCode);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "App";
+            this.Text = "App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtboxZipCode;
+        private System.Windows.Forms.Label lblEnter;
+    }
+}
+
---
+++ Temp.Application/App.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Temp.TemperatureServices;
+
+namespace Temp.App
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            btnSearch.Enabled = false;
+            txtboxZipCode.Enabled = false;
+
+            btnSearch.Text = "Loading...";
+
+            var zipCode = txtboxZipCode.Text;
+
+            var ow = new OpenWeatherTemperatureService(ConfigurationSettings.AppSettings["OpenWeatherAppId"]);
+            var service = new TemperatureService(ow);
+
+
+            var temp = await service.GetTemperatureAsync(Int32.Parse(zipCode));
+
+            btnSearch.Enabled = true;
+            txtboxZipCode.Enabled = true;
+            btnSearch.Text = "Search";
+            MessageBox.Show(temp.Temp.ToString());
+        }
+
+        private void lblEnterZipCode_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
